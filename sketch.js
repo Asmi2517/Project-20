e834ed20e335bd83106cf34bc2654862e9161707
Asmi2517 <64598278+Asmi2517@users.noreply.github.com>
2020-05-25 19:15:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -19,22 +19,19 @@ function draw() {
 
   car.velocityX = speed;
 
-  if (wall.x - car.x < wall.width/2 + car.width/2
-    && 0.5*speed*speed/22500 < 100) {
-    car.x = 1300;
-    car.velocityX = 0; 
-    car.shapeColor = color(0,255,0);}
-    else if (wall.x - car.x < wall.width/2 + car.width/2
-    && 0.5*speed*speed/22500 > 100
-    && 0.5*speed*speed/22500 < 180) {
-    car.x = 1300;
-    car.velocityX = 0; 
-    car.shapeColor = color(230,230,0);}
-    else if (wall.x - car.x < wall.width/2 + car.width/2
-      && 0.5*speed*speed/22500 > 180) {
-      car.x = 1300;
-      car.velocityX = 0; 
-      car.shapeColor = color(255,0,0);}
-      
+  if (wall.x - car.x < (car.width + wall.width)/2) {
+    car.velocityX = 0;
+    var deformation = 0.5 * weight * speed * speed/22500;
+    if (deformation > 180) {
+      car.shapeColor = color(255,0,0);
+    }
+    if (deformation < 180 && deformation > 100) {
+      car.shapeColor = color(230,230,0);
+    } 
+    if (deformation < 100) {
+      car.shapeColor = color(0,255,0);
+    }
+  }
+
   drawSprites();
 }
